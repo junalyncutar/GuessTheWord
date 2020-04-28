dd56a57b8d910233be5b7ccf304c9ba2e60159fd
cutarj <junalyn.cutar@ddp.kyocera.com>
2020-04-28 10:12:46 +0800

    Android Kotlin Fundamentals 05.2: LiveData and LiveData observers

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,21 +1,37 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    // The current word
-    var word = ""
+    // The current _word
+    private val _word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
-    // The list of words - the front of the list is the next word to guess
+    val word: LiveData<String>
+        get() = _word
+
+
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
+
+    // The list of words - the front of the list is the next _word to guess
     private lateinit var wordList: MutableList<String>
 
 
-    // Resets the list of words and randomizes the order
+    /**
+     * Resets the list of words and randomizes the order
+     */
     private fun resetList() {
         wordList = mutableListOf(
                 "queen",
@@ -44,32 +60,54 @@ class GameViewModel : ViewModel() {
     }
 
     init {
+        _word.value = ""
+        _score.value = 0
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
     }
 
-    // Callback called when the ViewModel is destroyed
+    /**
+     * Callback called when the ViewModel is destroyed
+     */
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 
-    // Methods for updating the UI
+    /** Methods for updating the UI **/
     fun onSkip() {
-        score--
+        _score.value = (_score.value)?.minus(1)
         nextWord()
     }
     fun onCorrect() {
-        score++
+        _score.value = (_score.value)?.plus(1)
         nextWord()
     }
 
-    // Moves to the next word in the list.
+    /**
+     * Moves to the next _word in the list.
+     */
     private fun nextWord() {
-        //Select and remove a word from the list
-        if (!wordList.isEmpty()) {
-            word = wordList.removeAt(0)
+        if (wordList.isEmpty()) {
+            onGameFinish()
+
+        } else {
+            //Select and remove a _word from the list
+            _word.value = wordList.removeAt(0)
         }
     }
+
+
+
+    /** Method for the game completed event **/
+
+    fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
+
 }
